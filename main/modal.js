5731b669e247b1a4cc885588e26fc6db340222be
74ultra <r.barnhill@protonmail.com>
2020-07-29 01:00:36 -0700

    Remove comments

--- main/modal.js
+++ main/modal.js
@@ -19,14 +19,3 @@ function openModal() {
     modal.style.display = 'block';
     backdrop.style.display = 'block';
 }
-
-
-
-/*
-
-In the Game of Life, these rules examine each cell of the grid. For each cell, it counts that cell's eight neighbors (up, down, left, right, and diagonals), and then act on that result.
-
-If the cell is alive and has 2 or 3 neighbors, then it remains alive. Else it dies.
-If the cell is dead and has exactly 3 neighbors, then it comes to life. Else if remains dead.
-
-*/
